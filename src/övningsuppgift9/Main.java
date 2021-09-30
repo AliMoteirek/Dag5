513f58103421726cc24c67d3eefcfec88ce1aa85
AliMoteirek <91534405+AliMoteirek@users.noreply.github.com>
2021-09-30 10:57:28 +0200

    h

--- src/övningsuppgift9/Main.java
+++ src/övningsuppgift9/Main.java
@@ -2,8 +2,14 @@ package övningsuppgift9;
 
 public class Main {
     public static void main(String[] args) {
-        for (RomerskaSiffror r : RomerskaSiffror.values()) {
-            System.out.println(r + " is \'" + r.getNum() + "\': " + r.getNumString());
+
+        RomerskaSiffror [] allRomerskaSiffror = RomerskaSiffror.values() ;
+        for (RomerskaSiffror romerskaSiffror : allRomerskaSiffror) {
+            System.out.println(romerskaSiffror + " is \'" + romerskaSiffror.getNum() + "\': " + romerskaSiffror.getNumString());
         }
+
+       // for (RomerskaSiffror r : RomerskaSiffror.values()) {
+       //     System.out.println(r + " is \'" + r.getNum() + "\': " + r.getNumString());
+       // }
     }
 }
